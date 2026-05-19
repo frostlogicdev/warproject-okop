d3d5302b2bccd007f04da874e8341de26c4a257e
frost logic <minertema69@gmail.com>
2026-05-19 15:06:24 +0300

    feat(docker): entrypoint with tuned G1 flags

---
+++ docker/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+set -e
+
+if [ "$EULA" != "true" ]; then
+  echo "You must accept the Minecraft EULA by setting EULA=true."
+  echo "See: https://www.minecraft.net/en-us/eula"
+  exit 1
+fi
+
+echo "eula=true" > eula.txt
+
+DEFAULT_OPTS="-Xmx4G -Xms4G \
+  -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 \
+  -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC \
+  -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 \
+  -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 \
+  -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 \
+  -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 \
+  -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1"
+
+OPTS="${JVM_OPTS:-$DEFAULT_OPTS}"
+
+if [ ! -f server.jar ] && [ -f run.sh ]; then
+  exec sh run.sh nogui
+fi
+
+exec java $OPTS -jar server.jar nogui
